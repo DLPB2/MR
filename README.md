3c51724f2e186e6ba2639b24a41889cfeec9f23e
DLPB2 <d71983@outlook.com>
2026-08-09 13:12:08 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Final Fantasy VII field archive editor ([Forum](http://forums.qhimm.com/index.ph
 
 ## Installing
 
-[![Stable Release](https://img.shields.io/github/downloads/myst6re/makoureactor/2.0.0/total?logo=github&label=Download%20Stable%20Release)](https://github.com/myst6re/makoureactor/releases/tag/2.0.0) [![Continious Release](https://img.shields.io/github/downloads/myst6re/makoureactor/continuous/total?logo=github&label=Download%20Continuous%20Release)](https://github.com/myst6re/makoureactor/releases/tag/continuous) [![Get From Flathub](https://img.shields.io/flathub/downloads/io.github.myst6re.makoureactor?logo=flathub&label=Download%20From%20Flathub)](https://flathub.org/apps/details/io.github.myst6re.makoureactor)
+[![Stable Release](https://img.shields.io/github/downloads/myst6re/makoureactor/2.0.0/total?logo=github&label=Download%20Stable%20Release)](https://github.com/myst6re/makoureactor/releases/tag/2.0.0) [![Continious Release](https://img.shields.io/github/downloads/myst6re/makoureactor/continuous/total?logo=github&label=Download%20Continuous%20Release)](https://github.com/myst6re/makoureactor/releases/tag/continuous)
 
 ## Contributing
 
@@ -18,101 +18,38 @@ PR on [GitHub](https://github.com/myst6re/makoureactor).
 
 ## Building
 
-If you are a developer and you want to build this software, follow the
-instructions below.
+Makou Reactor is built and supported as a **64-bit Windows application**.
+The supported toolchain is Qt 6 with the MSVC 2022 64-bit kit.
 
 ### Requirements
 
-- [zlib](http://www.zlib.net/)
-- [ff7tk](https://www.github.com/sithlord48/ff7tk)
-- CMake
-- Qt 6.2+
+- CMake 3.25+
+- Qt 6 with the MSVC 2022 64-bit kit and Qt 5 Compatibility Module
+- Visual Studio with the Desktop development with C++ workload
+- vcpkg (the repository submodule or a local checkout)
 
-### Windows
+### Qt + Qt Creator
 
-#### Qt + Qt Creator
+1. Install Qt 6 with `MSVC 2022 64-bit`, `Qt Creator`, and `Qt 5 Compatibility Module`.
+2. Install CMake and Ninja from the Qt installer developer tools.
+3. Open the repository `CMakeLists.txt` in Qt Creator and use a 64-bit MSVC kit.
 
-0. Download the online installer from https://www.qt.io/download-qt-installer
-1. Install Qt 6 with these items checked:
-   - **Qt/Qt 6.x.x (last version):** Check `MSVC 2022 64-bit`, `Qt Creator`, `Qt Shader Tools` and `Qt 5 Compatibility Module`
-   - **Developer And Designer Tools:** Check `CMake` and `Ninja`
-2. Run Qt Creator and open an existing project, select the CMakeLists.txt in the source directory of Makou Reactor
+### Visual Studio 2022
 
-#### Visual Studio
+1. Install Qt as described above.
+2. Import `.vsconfig` in the Visual Studio installer.
+3. Make sure the English language pack is installed.
+4. Open this repository **as a folder** in Visual Studio 2022.
+5. Select one of the CMake presets and build. The presets force the `x64-windows` vcpkg target and host triplets.
 
-> **Please note:**
->
-> By default Visual Studio will pick the **x86-Release** build configuration, but you can choose any other profile available.
+### Visual Studio Code
 
-0. **REQUIRED!** Follow the steps to install Qt, if you didn't already
-1. Download the the latest [Visual Studio Community](https://visualstudio.microsoft.com/vs/community/) installer
-2. Run the installer and import this [.vsconfig](.vsconfig) file in the installer to pick the required components to build this project
-3. Make sure you select the English Language pack in the language list before clicking Install
-4. Once installed, open this repository **as a folder** in Visual Studio 2022 and click the build button.
+1. Install Qt and Visual Studio as described above.
+2. Install the Microsoft C/C++ and CMake Tools extensions.
+3. Open this repository as a folder.
+4. Select a CMake preset such as `Release`.
+5. Configure and build.
 
-#### Optional: Visual Studio Code
+### GitHub Actions
 
-0. **REQUIRED!** Follow the steps to install Qt, if you didn't already
-1. **REQUIRED!** Follow the steps to install Visual Studio, if you didn't already
-2. Download and install the latest [Visual Studio Code](https://code.visualstudio.com/) release
-3. Install the following extensions:
-   - https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools
-   - https://marketplace.visualstudio.com/items?itemName=ms-vscode.cmake-tools
-4. Open this repository as a folder in Visual Studio code
-5. Choose as build profile in the status bar `CMake: [Release]` (or one of the aforementioned profiles)
-6. Click the button on the status bar `Build`
-
-### macOS
-
-In order to continue please make sure you have `brew` installed. See https://brew.sh/ for instructions.
-
-#### Install dependencies
-
-```sh
-$ brew install cmake
-$ brew install qt6
-```
-
-Close and reopen your terminal.
-
-#### Build
-
-> **Please note**: You can choose other build profiles if you want. See https://cmake.org/cmake/help/latest/variable/CMAKE_BUILD_TYPE.html for more information.
-
-```sh
-$ mkdir -p .dist/build .dist/install
-$ cmake -S . -B .dist/build -DCMAKE_INSTALL_PREFIX=.dist/install -DCMAKE_BUILD_TYPE=Release
-$ cmake --build .dist/build --config Release
-```
-
-### Linux
-
-#### Install dependencies
-
-##### Debian/Ubuntu
-
-```sh
-$ apt-get install build-essential cmake qt6-default qttools6-dev
-```
-
-##### Arch Linux
-
-```sh
-$ pacman -S --needed base-devel cmake qt6
-```
-
-#### Build
-
-> **Please note**: You can choose other build profiles if you want. See https://cmake.org/cmake/help/latest/variable/CMAKE_BUILD_TYPE.html for more information.
-
-```sh
-$ mkdir -p .dist/build .dist/install
-$ cmake -S . -B .dist/build -DCMAKE_INSTALL_PREFIX=.dist/install -DCMAKE_BUILD_TYPE=Release
-$ cmake --build .dist/build --config Release
-```
-
-#### Install
-
-```sh
-$ cmake --build .dist/build --target install
-```
+`.github/workflows/build.yml` builds the Windows x64 GUI package with MSVC, Qt 6, Ninja, CMake, and the `x64-windows` vcpkg triplet.
--- qt.cmake
+++ qt.cmake
@@ -6,116 +6,72 @@
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
+if(NOT WIN32)
+    message(FATAL_ERROR "Qt auto-detection is configured for Windows x64 only.")
+endif()
+
 if(NOT DEFINED QT_VERSION_TO_FIND)
-    SET(QT_VERSION_TO_FIND 6)
+    set(QT_VERSION_TO_FIND 6)
+endif()
+if(NOT QT_VERSION_TO_FIND EQUAL 6)
+    message(FATAL_ERROR "Makou Reactor requires Qt 6.")
 endif()
 
 find_package(QT NAMES Qt6 QUIET)
 
-# Auto-Detect Qt installation
 if(NOT QT_FOUND)
     find_program(QT_QMAKE_EXECUTABLE "qmake")
 
-    if(NOT QT_QMAKE_EXECUTABLE MATCHES "QT_QMAKE_EXECUTABLE-NOTFOUND")
-        # ../../
+    if(QT_QMAKE_EXECUTABLE)
         get_filename_component(QT_PATH "${QT_QMAKE_EXECUTABLE}" DIRECTORY)
         get_filename_component(QT_PATH "${QT_PATH}" DIRECTORY)
-
-        message("-- Found qmake in PATH: ${QT_QMAKE_EXECUTABLE}")
+        message(STATUS "Found qmake in PATH: ${QT_QMAKE_EXECUTABLE}")
     else()
-        if(WIN32)
-            # look for user-registry pointing to Qt installation
-            cmake_host_system_information(RESULT QT_INSTALLATION_PATH QUERY WINDOWS_REGISTRY "HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/Uninstall/{1f0b01f1-fb8c-49bb-8410-ef0628043911}" VALUE "InstallLocation")
-
-            if(QT_INSTALLATION_PATH STREQUAL "")
-                # look for user-registry pointing to qtcreator
-                cmake_host_system_information(RESULT QT_INSTALLATION_PATH QUERY WINDOWS_REGISTRY "HKEY_CURRENT_USER/Software/Classes/Applications/QtProject.QtCreator.qml/shell/Open/Command")
-                string(REPLACE " -client \"%1\"" "" QT_INSTALLATION_PATH ${QT_INSTALLATION_PATH})
-
-                if(QT_INSTALLATION_PATH STREQUAL "")
-                    message(FATAL_ERROR "Please set QT_INSTALLATION_PATH or Qt${QT_VERSION_TO_FIND}_DIR")
-                endif()
-
-                message("-- QtCreator Auto-Detected at ${QT_INSTALLATION_PATH}")
-
-                # ../../../../
-                get_filename_component(QT_INSTALLATION_PATH "${QT_INSTALLATION_PATH}" DIRECTORY)
-                get_filename_component(QT_INSTALLATION_PATH "${QT_INSTALLATION_PATH}" DIRECTORY)
-                get_filename_component(QT_INSTALLATION_PATH "${QT_INSTALLATION_PATH}" DIRECTORY)
-                get_filename_component(QT_INSTALLATION_PATH "${QT_INSTALLATION_PATH}" DIRECTORY)
-            endif()
-        elseif(NOT QT_INSTALLATION_PATH)
-            message(FATAL_ERROR "Please set QT_INSTALLATION_PATH or Qt${QT_VERSION_TO_FIND}_DIR")
+        if(NOT QT_INSTALLATION_PATH)
+            cmake_host_system_information(
+                RESULT QT_INSTALLATION_PATH
+                QUERY WINDOWS_REGISTRY
+                    "HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/Uninstall/{1f0b01f1-fb8c-49bb-8410-ef0628043911}"
+                VALUE "InstallLocation"
+            )
         endif()
 
-        file(GLOB QT_VERSIONS "${QT_INSTALLATION_PATH}/${QT_VERSION_TO_FIND}.*")
-        list(SORT QT_VERSIONS COMPARE NATURAL)
+        if(NOT QT_INSTALLATION_PATH)
+            message(FATAL_ERROR "Set QT_INSTALLATION_PATH or Qt6_DIR to a Qt 6 MSVC x64 installation.")
+        endif()
 
-        # assume the latest version will be last alphabetically
-        list(REVERSE QT_VERSIONS)
-
+        file(GLOB QT_VERSIONS "${QT_INSTALLATION_PATH}/6.*")
+        list(SORT QT_VERSIONS COMPARE NATURAL ORDER DESCENDING)
+        if(NOT QT_VERSIONS)
+            message(FATAL_ERROR "No Qt 6 installation found under ${QT_INSTALLATION_PATH}.")
+        endif()
         list(GET QT_VERSIONS 0 QT_VERSION)
 
-        message("-- Last Qt version ${QT_VERSION}")
-
-        if(MSVC)
-            if(MSVC_TOOLSET_VERSION MATCHES 143)
-                set(QT_MSVC "2022")
-            elseif(MSVC_TOOLSET_VERSION MATCHES 142)
-                set(QT_MSVC "2019")
-            elseif(MSVC_TOOLSET_VERSION MATCHES 141)
-                set(QT_MSVC "2017")
-            endif()
-
-            set(QT_PATH "${QT_VERSION}/msvc${QT_MSVC}_64")
-        elseif(MINGW)
-            file(GLOB QT_COMPILERS "${QT_VERSION}/mingw*")
-            list(SORT QT_COMPILERS)
-            list(REVERSE QT_COMPILERS)
-            list(GET QT_COMPILERS 0 QT_PATH)
-        else()
-            # For some reason GLOB also list files on Mac OSX
-            file(GLOB QT_COMPILERS "${QT_VERSION}/*/lib")
-            list(SORT QT_COMPILERS)
-            list(REVERSE QT_COMPILERS)
-            list(GET QT_COMPILERS 0 QT_PATH)
-            # ../
-            get_filename_component(QT_PATH "${QT_PATH}" DIRECTORY)
+        file(GLOB QT_COMPILERS "${QT_VERSION}/msvc*_64")
+        list(SORT QT_COMPILERS COMPARE NATURAL ORDER DESCENDING)
+        if(NOT QT_COMPILERS)
+            message(FATAL_ERROR "No Qt 6 MSVC x64 kit found under ${QT_VERSION}.")
         endif()
-
-        set(QT_QMAKE_EXECUTABLE "${QT_PATH}/bin/qmake")
+        list(GET QT_COMPILERS 0 QT_PATH)
+        set(QT_QMAKE_EXECUTABLE "${QT_PATH}/bin/qmake.exe")
     endif()
 
-    set(Qt${QT_VERSION_TO_FIND}_DIR "${QT_PATH}/lib/cmake/Qt${QT_VERSION_TO_FIND}")
-    set(QT_DIR "${Qt${QT_VERSION_TO_FIND}_DIR}")
-
-    message("-- Qt${QT_VERSION_TO_FIND}_DIR: ${Qt${QT_VERSION_TO_FIND}_DIR}")
-
-    find_package(QT NAMES Qt${QT_VERSION_TO_FIND} REQUIRED)
-
-    message("-- Qt Auto-Detected at ${QT_PATH}")
+    set(Qt6_DIR "${QT_PATH}/lib/cmake/Qt6")
+    set(QT_DIR "${Qt6_DIR}")
+    find_package(QT NAMES Qt6 REQUIRED)
 endif()
 
-if(NOT DEFINED QT_QMAKE_EXECUTABLE OR QT_QMAKE_EXECUTABLE MATCHES "QT_QMAKE_EXECUTABLE-NOTFOUND")
-    find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Core REQUIRED)
-    get_target_property(QT_QMAKE_EXECUTABLE Qt${QT_VERSION_MAJOR}::qmake IMPORTED_LOCATION)
+if(NOT DEFINED QT_QMAKE_EXECUTABLE OR NOT EXISTS "${QT_QMAKE_EXECUTABLE}")
+    find_package(Qt6 COMPONENTS Core REQUIRED)
+    get_target_property(QT_QMAKE_EXECUTABLE Qt6::qmake IMPORTED_LOCATION)
 endif()
-message("-- QT_QMAKE_EXECUTABLE: ${QT_QMAKE_EXECUTABLE}")
+
 get_filename_component(_qt_bin_dir "${QT_QMAKE_EXECUTABLE}" DIRECTORY)
-get_filename_component(_qt_dir "${_qt_bin_dir}" DIRECTORY)
-set(_qt_translations_dir "${_qt_dir}/translations")
-
-# Add the QT_PATH to the resolution paths
+get_filename_component(QT_PATH "${_qt_bin_dir}" DIRECTORY)
 list(APPEND CMAKE_PREFIX_PATH "${QT_PATH}")
+set(QT_PATH "${QT_PATH}" CACHE PATH "Path to the Qt 6 Windows x64 kit")
 
-set(QT_PATH "${QT_PATH}" CACHE PATH "Path to QT directory")
+message(STATUS "QT_QMAKE_EXECUTABLE: ${QT_QMAKE_EXECUTABLE}")
+message(STATUS "Qt Windows x64 path: ${QT_PATH}")
